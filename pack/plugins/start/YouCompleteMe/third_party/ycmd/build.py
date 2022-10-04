69cd697e291f03567475bf6499db149bdf56ecff
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 18:11:28 +0000

    Adding numeric_owner as keyword arguement

--- pack/plugins/start/YouCompleteMe/third_party/ycmd/build.py
+++ pack/plugins/start/YouCompleteMe/third_party/ycmd/build.py
@@ -723,7 +723,7 @@ def EnableJavaCompleter( switches ):
             if not is_within_directory(path, member_path):
                 raise Exception("Attempted Path Traversal in Tar File")
     
-        tar.extractall(path, members, numeric_owner) 
+        tar.extractall(path, members, numeric_owner=numeric_owner) 
         
     
     safe_extract(package_tar, REPOSITORY)
